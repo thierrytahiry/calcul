18951f34b9a10da5ac46ae7b68eca10e7b230abb
Tahiry Thierry <tahirythierry@gmail.com>
2022-09-02 08:52:30 +0300

    nombre boucle message

---
+++ nbboucle.php
@@ -0,0 +1,13 @@
+<?php
+
+$message = 'Je me rèpète là, non? <br>';
+
+for($boucle1 = 1; $boucle1 <=20;$boucle1++)
+{
+    for($boucle2 = 1;$boucle2<=10; $boucle2++);
+    {
+        echo $message;//la message affiche 20 fois
+    }
+}
+
+
